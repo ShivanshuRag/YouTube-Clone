33df5fa4b3e105217d04126e798f66ff66d80447
ShivanshuRag <ragsingh773@gmail.com>
2023-09-20 21:32:51 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let menuIcon = document.querySelector("i")
+let sidebar = document.querySelector(".sidebar")
+let container = document.querySelector(".container")
+let tags =  document.querySelector(".tags")
+
+menuIcon.onclick = function(){
+
+  sidebar.classList.toggle("small-sidebar")
+  // sidebar.classList.toggle("large-sidebar")
+
+  container.classList.toggle("large-container")
+  // tags.classList.toggle("-tags")
+  
+}
+ 
+let suscribeButton = document.querySelector("#btn")
+
+btn.addEventListener("click", ()=>{
+
+  btn.style.backgroundColor="black"
+
+
+})
+
+
+ 
+
